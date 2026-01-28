aea77d90f3c87a27a693cfd0333487945b5ad0f6
gaurav <gauravgs.1290@gmail.com>
2026-01-28 13:19:05 +0530

    api fix

--- Backend/utils/mailSender.js
+++ Backend/utils/mailSender.js
@@ -5,7 +5,7 @@ exports.mailSender = async (to, subject, html) => {
     const transporter = nodemailer.createTransport({
       host: process.env.MAIL_HOST,
       port: process.env.MAIL_PORT,
-      secure: false, // true only for port 465
+      secure: false, 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
