2dd8d4201676f58f066d1a2643f131f65af09a3b
gaurav <gauravgs.1290@gmail.com>
2026-01-28 13:36:55 +0530

    fix api.js

--- Frontend/vite-project/src/services/api.js
+++ Frontend/vite-project/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-
-const BASE_URL = import.meta.env.BACKEND_URL;
+const BASE_URL = "http://localhost:5000";
 
 export const axiosInstance = axios.create({
     baseURL:BASE_URL,
