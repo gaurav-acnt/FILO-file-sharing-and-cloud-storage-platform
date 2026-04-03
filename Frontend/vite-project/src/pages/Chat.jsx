ed0e0198f2a6f1639c2e657683080ae27bd7feb7
gaurav <gauravgs.1290@gmail.com>
2026-04-03 11:29:26 +0530

    socket url change

--- Frontend/vite-project/src/pages/Chat.jsx
+++ Frontend/vite-project/src/pages/Chat.jsx
@@ -3,7 +3,7 @@ import toast from "react-hot-toast";
 import { axiosInstance, authHeader, getToken } from "../services/api";
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.BACKEND_SOCKET_URL;
+const SOCKET_URL = import.meta.env.VITE_BACKEND_SOCKET_URL; 
 
 export default function Chat() {
   const user = useMemo(() => {
