0838366ebe5464e8e928c2bb8db3ceeec2fa2a8b
Gaurav <gouravgs.1290@gmail.com>
2026-04-26 11:33:34 +0530

    Update api.js

--- Frontend/vite-project/src/services/api.js
+++ Frontend/vite-project/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.VITE_BACKEND_URL ||
-  "https://filo-backend-production.up.railway.app"; 
+  "https://filo-backend-e49y.onrender.com"; 
 
 export const axiosInstance = axios.create({
     baseURL:BASE_URL,
